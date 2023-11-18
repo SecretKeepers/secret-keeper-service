0331573408dbd69cdc690ecc25e2e5fd76a586bb
Rachel <rachel.negi@sap.com>
2023-11-18 09:02:39 +0530

    constants test

---
+++ src/test/java/com/secretkeeper/constants/CryptoConstantsTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CryptoConstantsTest {
+
+    @Test
+    public void testCipherAlgorithm() {
+        assertThat(CryptoConstants.CIPHER_ALGORITHM.getStrValue()).isEqualTo("AES/GCM/NoPadding");
+    }
+
+    @Test
+    public void testIVSizeBytes() {
+        assertThat(CryptoConstants.IV_SIZE_BYTES.getIntValue()).isEqualTo(12);
+    }
+
+    @Test
+    public void testSaltSizeBytes() {
+        assertThat(CryptoConstants.SALT_SIZE_BYTES.getIntValue()).isEqualTo(16);
+    }
+
+    @Test
+    public void testTagSizeBits() {
+        assertThat(CryptoConstants.TAG_SIZE_BITS.getIntValue()).isEqualTo(128);
+    }
+
+    @Test
+    public void testSecretKeyAlgorithm() {
+        assertThat(CryptoConstants.SECRET_KEY_ALGORITHM.getStrValue()).isEqualTo("PBKDF2WithHmacSHA256");
+    }
+
+    @Test
+    public void testAES() {
+        assertThat(CryptoConstants.AES.getStrValue()).isEqualTo("AES");
+    }
+}
